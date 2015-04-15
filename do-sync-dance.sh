354d3e559d304d6bd456754dc81c31324816c4b6
wanderanimrod <wanderanimrod@gmail.com>
2015-04-15 16:32:17 +0300

    Fixes bnug

--- do-sync-dance.sh
+++ do-sync-dance.sh
@@ -2,4 +2,4 @@
 touch /home/pi/dropbox-sync-js/js-sync-log-$MACHINE_NAME
 echo `date` "Copying log file ... "
 cp /home/pi/dropbox-sync-js/js-sync-log-$MACHINE_NAME /media/ntfs/pi/rasppi/js-sync-log-$MACHINE_NAME
-sudo /home/pi/check-connection.sh && /usr/local/bin/node /home/pi/dropbox-sync-js/sync.js machine $MACHINE_NAME
+sudo /home/pi/dropbox-sync-js/check-connection.sh && /usr/local/bin/node /home/pi/dropbox-sync-js/sync.js machine $MACHINE_NAME
